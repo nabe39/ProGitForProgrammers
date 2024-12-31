358f8f6ce3aea7306cc0f017a6c49c973e06ee51
DucVo <vohongduc000@gmail.com>
2024-12-31 09:45:56 +0700

    add line to show the staging area in Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ public class Program
         Console.WriteLine("Hello World!");
         Console.WriteLine("I just added this in Visual Studio");
         Console.WriteLine("I just added this in the command line repo");
+        Console.WriteLine("This line added to show the staging area");
     }
    }
 }
