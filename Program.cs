43c4d734ef04952bcd6a6b9d7f7e4a7503244e0e
DucVo <vohongduc000@gmail.com>
2024-12-31 09:55:29 +0700

    Add calculator class

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
-public class Program
+namespace ProGitForProgrammers
 {
-   class Program{
-    static void Main(String []args){
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("I just added this in Visual Studio");
-        Console.WriteLine("I just added this in the command line repo");
-        Console.WriteLine("This line added to show the staging area");
+    public class Calculator
+    {
     }
-   }
 }
