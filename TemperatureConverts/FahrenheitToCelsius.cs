9dc137e753c57a5bce8b7294907c9875d29cbcc0
DucVo <vohongduc000@gmail.com>
2024-12-31 22:03:57 +0700

    add static method on FahrenheitToCelsiusConverter.cs

--- TemperatureConverts/FahrenheitToCelsius.cs
+++ TemperatureConverts/FahrenheitToCelsius.cs
@@ -4,8 +4,13 @@ namespace ProGitForProgrammers.Converters
     {
         public double FahrenheitToCelsiusConverter(double FahrenheitTemp)
         {
-            double _fahreneithTemp = 0.0;
-            double _celsius = 0.0;
+           double _celsius = 0.0;
+            // (F – 32) * 5/9
+            var step1 = Calculator.Calculator.Subtract(
+            fahrenheitTemp, 32);
+            var step2 = Calculator.Calculator.Multiplication(
+            step1, 5.0);
+            _celsius = Calculator.Calculator.Division(step2, 9.0);
             return _celsius;
         }
     }
