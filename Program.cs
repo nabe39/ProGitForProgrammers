47f62e89007db71399f672f370a67f2ed1578e69
DucVo <vohongduc000@gmail.com>
2024-12-31 22:07:34 +0700

    add skeletons to Main in Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ public class Program
    class Program{
     static void Main(string[] args)
     {
-        // skeleton program
+        var converter = new FahrenheitToCelsius();
+        var celsius = converter.FahrenheitToCelsiusConverter(212.0);
+        Console.WriteLine($"Fahrenheit temp of 212 is {celsius}.");
     }
    }
 }
