f4b38f5cdedc7bc53e52b7457c93867a0e9daffd
DucVo <vohongduc000@gmail.com>
2024-12-31 20:18:15 +0700

    change Program.cs for merge conflict  project

--- Program.cs
+++ Program.cs
@@ -3,10 +3,7 @@ public class Program
    class Program{
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("I just added this in Visual Studio");
-        Console.WriteLine("I just added this in the command line repo");
-        Console.WriteLine("This line added to show the staging area");
+        // skeleton program
     }
    }
 }
