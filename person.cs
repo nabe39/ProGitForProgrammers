bde879ac56ee33571269a74bd6aad04f19418dd9
DucVo <vohongduc000@gmail.com>
2025-01-02 11:54:00 +0700

    add weight property at person.css

--- person.cs
+++ person.cs
@@ -3,5 +3,6 @@ namespace ProGitForProgrammers{
          public double Age { get; set; }
          public string Name{get;set;}
          public double Height {get;set;}
+         public double Weight {get;set;}
     }
 }
