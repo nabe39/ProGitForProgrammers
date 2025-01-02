2716c751c81df33ac03fc3f3c2a634ec9503eb0f
DucVo <vohongduc000@gmail.com>
2025-01-02 11:32:48 +0700

    Add height property in person.css

--- person.cs
+++ person.cs
@@ -2,5 +2,6 @@ namespace ProGitForProgrammers{
     public class Person{
          public double Age { get; set; }
          public string Name{get;set;}
+         public double Height {get;set;}
     }
 }
