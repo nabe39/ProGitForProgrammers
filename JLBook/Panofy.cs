b9749a76462f61c99071bd354ef1b7775d30c09f
DucVo <vohongduc000@gmail.com>
2025-01-02 15:10:31 +0700

    create Class Panofy, class program, generate main function in Panofy.cs

---
+++ JLBook/Panofy.cs
@@ -0,0 +1,11 @@
+public class Panofy
+{
+   class Program{
+    static void Main(string[] args)
+    {
+        var converter = new FahrenheitToCelsius();
+        var celsius = converter.FahrenheitToCelsiusConverter(212.0);
+        Console.WriteLine($"Fahrenheit temp of 212 is {celsius}.");
+    }
+   }
+}
